ff1e6d1e4b9030673d259c4ed3439d55bdd96753
xiaochou <dunwenqiang@gmail.com>
2017-07-21 17:18:20 +0800

    【新增】=default关键字，显式指示编译器生成默认构造函数，保证了TwoCstor仍为POD类型。

---
+++ 7-2-1.cpp
@@ -0,0 +1,27 @@
+#include <type_traits>
+#include <iostream>
+
+using namespace std;
+
+class TwoCstor {
+public:
+    TwoCstor() = default;
+    TwoCstor(int i): data(i) {}
+
+private:
+    int data;
+};
+
+class NoCopyCstor {
+public:
+    NoCopyCstor() = default;
+private:
+    NoCopyCstor(const NoCopyCstor &);
+};
+
+int main() {
+    cout << is_pod<TwoCstor>::value << endl;
+
+    NoCopyCstor a;
+    NoCopyCstor b(a);
+}
